63c836aa167b6549b3730c7f7b9de6c8f1fae7fe
hwangsunteak <sunjaworld@naver.com>
2026-09-11 15:39:47 +0900

    되는거냐? 구조가 어떻게 되는거냐?

--- springboot-app/hello/src/main/java/com/example/GreetingController.java
+++ springboot-app/hello/src/main/java/com/example/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hello, %s! 설정대로 따라했음. 원리나 기술을 익히면될듯.";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
